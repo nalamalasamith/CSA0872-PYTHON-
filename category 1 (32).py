02283dfc8eba1ccd851fd81c8e08d29a5aa08b7f
nalamalasamith <141405644+nalamalasamith@users.noreply.github.com>
2024-06-07 09:06:43 +0530

    Add files via upload

---
+++ category 1 (32).py
@@ -0,0 +1,4 @@
+Python 2.7.18 (v2.7.18:8d21aa21f2, Apr 20 2020, 13:25:05) [MSC v.1500 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+========= RESTART: C:\Users\chint\Desktop\sandhya\category 1 (1).py =========
